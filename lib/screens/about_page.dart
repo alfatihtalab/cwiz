eacbfe68113e2f887a0384dfd8c26127ddcbf6b0
Alfatih <alfatihtalab7@gmail.com>
2021-06-08 07:10:19 +0200

    second_commit: updating ifo of the app and fix some errors. released app.

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -6,13 +6,41 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('CWIZ'),
+      ),
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [Text('About app'), Text('descriptions')],
+            children: [
+              Text(
+                'About app:',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                'This is simple weather app get your current location and send'
+                ' it as api-request form an open source, getting back with data of'
+                ' temperatures in celsius and some info of weather.\n \n'
+                'All rights reserved,, \n\n',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text('Developer contact:',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                  'Alfatih Abdelmutalib\n'
+                  'e-mail: alfatihtalab7@gmail.com \n'
+                  'phone: +249906078535',
+                  style: TextStyle(fontSize: 18))
+            ],
           ),
         ),
       ),
